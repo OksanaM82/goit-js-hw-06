89eaf1445bf3136220e460896aea9323877cf419
OksanaM82 <musijcukoksana82@gmail.com>
2023-01-16 17:55:20 +0200

    додає оформлення в галерею

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,21 +1,26 @@
+
+
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
 
-const ulElement = document.querySelector(".gallery");
-const listContent = images.map(
-    (image) =>
-      `<li> <img src=${image.url} alt=${image.alt}
-       width = 200 height = 150></li>`).join("");
-ulElement.insertAdjacentHTML("afterbegin", listContent);
+const createGalleryImage = ({ url, alt }) =>
+  `<li><img src="${url}" alt="${alt}" width = 400 ></li>`;
+const galleryItems = images.reduce(
+  (image, item) => image + createGalleryImage(item),
+  ""
+);
+const galleryList = document.querySelector(".gallery");
+galleryList.insertAdjacentHTML("beforeend", galleryItems);
+galleryList.setAttribute("style", "list-style-type:none; display: flex; gap: 10px;");
--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,9 +1,9 @@
 const textInput = document.querySelector("#validation-input");
 const textInputLength = Number(textInput.dataset.length);
 textInput.addEventListener("blur", (event) => {
-  if (event.currentTarget.value.length === textInputLength) {
-    textInput.classList.add("valid");
+  if (event.currentTarget.value.length !== textInputLength) {
+    textInput.classList.add("invalid");
   } else {
-    textInput.classList.replace("valid", "invalid");
+    textInput.classList.replace("invalid", "valid");
   }
 });
