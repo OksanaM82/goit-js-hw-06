eace3197bf881d5711aafbe2aef4c8cf89663c13
OksanaM82 <musijcukoksana82@gmail.com>
2023-01-18 00:32:16 +0200

    додає виправлення

--- js/task-02.js
+++ js/task-02.js
@@ -6,10 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const ulElement = document.getElementById("ingredients"); 
-
-const ingredientsList = ingredients.forEach((ingredient) => {
+const ulElement = document.getElementById("ingredients");  
+const ingredientsList = ingredients.map((ingredient) => {
   let items = document.createElement("li");
+  items.classList.add("item");
   items.innerHTML = ingredient;
   ulElement.append(items);
   
--- js/task-10.js
+++ js/task-10.js
@@ -4,4 +4,23 @@ function getRandomHexColor() {
 const createBtn = document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
 const boxes = document.getElementById("boxes");
-createBtn
+createBtn.addEventListener("click", addBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+function createBoxes(amount) {
+  const baseSize = 30;
+  const boxSize = baseSize + i * 10;
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.cssText = `width: ${boxSize}px;
+     hight: ${boxSize}px;
+      bacgraundcor: ${getRandomHexColor()};`
+    boxes.appendChild(box);
+  }
+}
+  function addBoxes () {
+    const amount = document.querySelector("input").value;
+    createBoxes(amount);
+}
+function destroyBoxes () {
+  boxes.innerHTML = "";
+} 
